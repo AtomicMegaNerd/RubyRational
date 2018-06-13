e2ceea884ff756376e453ec2cda56e69ebcefd11
Chris Dunphy <chris.dunphy@fglsports.com>
2018-06-13 16:51:12 -0600

    Formatting with Rufo

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require_relative 'rational'
+require_relative "rational"
 
 include Rcd
 
--- rational.rb
+++ rational.rb
@@ -1,61 +1,60 @@
 module Rcd
+  def gcd(x, y)
+    if (y == 0)
+      return x
+    else
+      return gcd(y, x % y)
+    end
+  end
 
-    def gcd(x, y)
-        if (y == 0)
-            return x
-        else
-            return gcd(y, x % y)
-        end
+  def lcm(x, y)
+    (x * y) / gcd(x, y)
+  end
+
+  class RcdRational
+    def initialize(numer, denom)
+      @numer = numer
+      @denom = denom
+      reduce()
     end
 
-    def lcm(x, y)
-        (x * y) / gcd(x, y)
+    def reduce()
+      g = gcd(@numer, @denom)
+      @numer /= g
+      @denom /= g
     end
 
-    class RcdRational
-        def initialize(numer, denom)
-            @numer = numer
-            @denom = denom
-            reduce()
-        end
-
-        def reduce()
-            g = gcd(@numer, @denom)
-            @numer /= g
-            @denom /= g
-        end
-
-        def reciprocal()
-            RcdRational.new(@denom, @numer)
-        end
-
-        def to_s()
-            "#{@numer}/#{@denom}"
-        end
-
-        def +(other)
-            l = lcm(@denom, other.denom)
-            RcdRational.new(
-                (@numer * l / @denom) + (other.numer * l / other.denom), l
-            )
-        end
-
-        def -(other)
-            l = lcm(@denom, other.denom)
-            RcdRational.new(
-                (@numer * l / @denom) - (other.numer * l / other.denom), l
-            )
-        end
-
-        def *(other)
-            RcdRational.new(@numer * other.numer, @denom * other.denom)
-        end
-
-        def /(other)
-            self * other.reciprocal
-        end
-
-        attr_accessor :numer
-        attr_accessor :denom
+    def reciprocal()
+      RcdRational.new(@denom, @numer)
     end
+
+    def to_s()
+      "#{@numer}/#{@denom}"
+    end
+
+    def +(other)
+      l = lcm(@denom, other.denom)
+      RcdRational.new(
+        (@numer * l / @denom) + (other.numer * l / other.denom), l
+      )
+    end
+
+    def -(other)
+      l = lcm(@denom, other.denom)
+      RcdRational.new(
+        (@numer * l / @denom) - (other.numer * l / other.denom), l
+      )
+    end
+
+    def *(other)
+      RcdRational.new(@numer * other.numer, @denom * other.denom)
+    end
+
+    def /(other)
+      self * other.reciprocal
+    end
+
+    attr_accessor :numer
+    attr_accessor :denom
+  end
 end
